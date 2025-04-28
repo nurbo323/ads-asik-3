103102820a62bd73ce08cab815ecf8d06f73ec5e
nurbo323 <242214@astanait.edu.kz>
2025-04-28 12:01:17 +0500

    test: add Main.java to test MyHashTable with 10000 elements

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,22 @@
 package org.example;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, ADS Project!");
+        System.out.println("--- Testing MyHashTable ---");
+
+        MyHashTable<MyTestingClass, Integer> table = new MyHashTable<>();
+        Random random = new Random();
+
+        for (int i = 0; i < 10000; i++) {
+            int id = random.nextInt(100000);
+            String name = "Name" + random.nextInt(100000);
+            MyTestingClass key = new MyTestingClass(id, name);
+            table.put(key, i);
+        }
+
+        System.out.println("Total elements inserted: " + table.getSize());
+        table.printBucketSizes();
     }
 }
-
